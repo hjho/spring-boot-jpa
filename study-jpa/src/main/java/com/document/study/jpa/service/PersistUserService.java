a44b7b933d26b9670b85969d45fbaa48faa40432
hjho <gjwnsgh131@naver.com>
2026-03-01 17:48:12 +0900

    @Transactional 추가.

--- study-jpa/src/main/java/com/document/study/jpa/service/PersistUserService.java
+++ study-jpa/src/main/java/com/document/study/jpa/service/PersistUserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.document.study.jpa.entity.PersistUser;
 import com.document.study.jpa.repository.PersistUserRepository;
@@ -22,7 +23,7 @@ public class PersistUserService {
 	
 	private final PersistUserRepository persistUserRepository;
 	
-	
+	@Transactional
 	public boolean persist() {
 		long count = persistUserRepository.count();
 		if(count == 0) {
@@ -36,28 +37,31 @@ public class PersistUserService {
 		
 		return false;
 	}
-
-
+	
+	
+	@Transactional(readOnly = true)
 	public List<PersistUser> findUseClass() {
 		UserNameSpecification spec = new UserNameSpecification("생이");
 		List<PersistUser> list = persistUserRepository.findAll(spec);
 		
 		log.debug("## PersistUser findUseClass size: {}", list.size());
-        return list;
+		return list;
 	}
-
-
+	
+	
+	@Transactional(readOnly = true)
 	public List<PersistUser> findUseFunc() {
 		Specification<PersistUser> likeSpec = UserSpecs.nameLike("생이");
-        Specification<PersistUser> afterSpec = UserSpecs.createdAfter(LocalDateTime.now().minusHours(1));
-        Specification<PersistUser> allSpec = likeSpec.and(afterSpec);
-        List<PersistUser> list = persistUserRepository.findAll(allSpec);
+		Specification<PersistUser> afterSpec = UserSpecs.createdAfter(LocalDateTime.now().minusHours(1));
+		Specification<PersistUser> allSpec = likeSpec.and(afterSpec);
+		List<PersistUser> list = persistUserRepository.findAll(allSpec);
 		
 		log.debug("## PersistUser findUseFunc size: {}", list.size());
-        return list;
+		return list;
 	}
-
-
+	
+	
+	@Transactional(readOnly = true)
 	public List<PersistUser> findUseBuilder() {
 		
 		String text = "생이";
@@ -66,17 +70,18 @@ public class PersistUserService {
 		
 		Specification<PersistUser> specs = 
 				SpecBuilder.builder(PersistUser.class)
-	                	.ifHasText(text // text가 있으면.
-	                			, value -> UserSpecs.nameLike(value))
-	                	.ifNotNull(time // time이 null이 아니면.
-	                			, value -> UserSpecs.createdAfter(value))
-	                	.ifTrue(isYn // isYn 이 ture 이면.
-	                			, () -> UserSpecs.isNameTen())
-                	.build();
+				.ifHasText(text // text가 있으면.
+						, value -> UserSpecs.nameLike(value))
+				.ifNotNull(time // time이 null이 아니면.
+						, value -> UserSpecs.createdAfter(value))
+				.ifTrue(isYn // isYn 이 ture 이면.
+						, () -> UserSpecs.isNameTen())
+				.build();
 		
 		List<PersistUser> list = persistUserRepository.findAll(specs);
 		
 		log.debug("## PersistUser findUseFunc size: {}", list.size());
 		return list;
 	}
+	
 }
